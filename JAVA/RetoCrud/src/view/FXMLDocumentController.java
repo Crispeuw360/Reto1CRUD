cc844af418d0e7a57f972eeba6e95c6dc9ce2f2d
Crispeuw360 <igornikolaidis@gmail.com>
2025-10-17 10:50:11 +0200

    Ventana DropOut terminada

--- JAVA/RetoCrud/src/view/FXMLDocumentController.java
+++ JAVA/RetoCrud/src/view/FXMLDocumentController.java
@@ -10,26 +10,50 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
 
 /**
+ * FXML Controller class
  *
  * @author 2dami
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Button ConfirmBtn;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private Circle avatarIcon;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private Label passwordConfirmLabel;
+    @FXML
+    private PasswordField passwordConfirmField;
+    @FXML
+    private Label deleteLabel;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
     
 }
