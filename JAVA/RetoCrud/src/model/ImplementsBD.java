916915bda9cdfe7607072e4f6111ca223b135c4e
Pablo <pablovelas04@gmail.com>
2025-10-24 09:48:05 +0200

    merge

---
+++ JAVA/RetoCrud/src/model/ImplementsBD.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author pablo
+ */
+public class ImplementsBD implements WorkerDAO {
+    private Connection con;
+    private PreparedStatement stmt;
+    
+    private ResourceBundle configFile;
+    private String driverBD;
+    private String urlBD;
+    private String userBD;
+    private String passwordBD;
+    
+    
+    
+    
+    public ImplementsBD(){
+        this.configFile = ResourceBundle.getBundle("model.configClase");
+        this.driverBD = this.configFile.getString("Driver");
+        this.urlBD = this.configFile.getString("Conn");
+        this.userBD = this.configFile.getString("DBUser");
+        this.passwordBD = this.configFile.getString("DBPass");
+    }
+    
+     private void openConnection() {
+        try {
+            con = DriverManager.getConnection(urlBD, this.userBD, this.passwordBD);
+        } catch (SQLException e) {
+            System.out.println("Error al intentar abrir la BD");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+     private void closeConnection() {
+        try {
+            if (stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexión con la BD");
+            e.printStackTrace();
+        }
+    }
+     public boolean existsUsername(String username) {
+        boolean exists = false;
+
+        try {
+            openConnection();
+            String sql = "SELECT COUNT(*) FROM Profile_ WHERE user_name = ?";
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, username);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next() && rs.getInt(1) > 0) {
+                exists = true;
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println("❌ Error al comprobar si el usuario existe");
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+
+        return exists;
+    }
+
+     
+     public boolean insertUser(User_ user) {
+        boolean inserted = false;
+        try {
+            openConnection();
+
+            // Paso 1: Insertar en Profile_
+            String sqlProfile = "INSERT INTO Profile_ (user_name, passwd, email, name_, Surname, Telephone) VALUES (?,?,?,?,?,?)";
+            stmt = con.prepareStatement(sqlProfile, PreparedStatement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUser_name());
+            stmt.setString(2, user.getPasswd());
+            stmt.setString(3, user.getEmail());
+            stmt.setString(4, user.getName_());
+            stmt.setString(5, user.getSurname());
+            stmt.setInt(6, user.getTelephone());
+            stmt.executeUpdate();
+
+            // Obtener el ID generado
+            ResultSet rs = stmt.getGeneratedKeys();
+            int profileCode = 0;
+            if (rs.next()) {
+                profileCode = rs.getInt(1);
+            }
+            rs.close();
+            stmt.close();
+
+            // Paso 2: Insertar en User_
+            String sqlUser = "INSERT INTO User_ (Profile_code, card_no, gender) VALUES (?,?,?)";
+            stmt = con.prepareStatement(sqlUser);
+            stmt.setInt(1, profileCode);
+            stmt.setInt(2, user.getCard_no());
+            stmt.setString(3, user.getGender());
+
+            inserted = stmt.executeUpdate() > 0;
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return inserted;
+    }
+
+     
+}
--- JAVA/RetoCrud/src/view/SignUpController.java
+++ JAVA/RetoCrud/src/view/SignUpController.java
@@ -10,9 +10,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import model.ImplementsBD;
+import model.User_;
 
 /**
  * FXML Controller class
@@ -55,13 +58,148 @@ public class SignUpController implements Initializable {
     private Label nameLabel;
     @FXML
     private Button btnShow;
+    @FXML
+    private TextField fieldCard;
+    @FXML
+    private Label cardLabel;
+    @FXML
+    private Label genderLabel;
+    @FXML
+    private ComboBox<String> comboGender;
 
+    
+    
+    private ImplementsBD bd = new ImplementsBD(); // acceso a BD
+    private boolean passwordsVisible = false;    // para mostrar/ocultar contraseñas
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    comboGender.getItems().addAll("Male", "Female", "Other");
+    btnRegistro.setDisable(true);
+
+    // Cada vez que el usuario escribe, comprobamos si todos los campos están llenos
+    fieldUser.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldName.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldSurname.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldGmail.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldTel.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldPass.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldPass2.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    fieldCard.textProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    comboGender.valueProperty().addListener((obs, oldVal, newVal) -> checkFields());
+    }
+    
+   private void checkFields() {
+    boolean camposCompletos = true;
+
+    if (fieldUser.getText().isEmpty()) camposCompletos = false;
+    if (fieldName.getText().isEmpty()) camposCompletos = false;
+    if (fieldSurname.getText().isEmpty()) camposCompletos = false;
+    if (fieldGmail.getText().isEmpty()) camposCompletos = false;
+    if (fieldTel.getText().isEmpty()) camposCompletos = false;
+    if (fieldPass.getText().isEmpty()) camposCompletos = false;
+    if (fieldPass2.getText().isEmpty()) camposCompletos = false;
+    if (fieldCard.getText().isEmpty()) camposCompletos = false;
+    if (comboGender.getValue() == null) camposCompletos = false;
+
+    btnRegistro.setDisable(!camposCompletos);
+    
+    
+   }
+   
+   @FXML
+    public void onRegister() {
+        boolean datosValidos=false;
+        int tel = 0;
+        int card = 0;
+        String porque="Nada";
+        
+        if (fieldPass.getText().equals(fieldPass2.getText())){
+            datosValidos=true;
+            if (!fieldGmail.getText().matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")){
+                datosValidos=false;
+                porque="Gmail";
+                showError("Gmail invalido");
+            }
+            else{
+                datosValidos=true;
+                try {
+                    tel = Integer.parseInt(fieldTel.getText());
+                    card = Integer.parseInt(fieldCard.getText());
+                } catch (NumberFormatException e) {
+                    datosValidos = false;
+                    showError("Telefono o numero de tarjeta no numericos");
+                    porque = "Teléfono o número de tarjeta no válidos (deben ser numéricos)";
+                    
+                }
+            }
+        }
+        else{
+            datosValidos=false;
+            
+            porque="Pass invalida";
+            showError("Contraseñas no coinciden");
+        }
+        System.out.println("Datos Validos: "+datosValidos);
+        System.out.println(porque);
+        
+        if (datosValidos) {
+            User_ nuevoUser = new User_(
+                0,
+                fieldUser.getText(),
+                fieldPass.getText(),
+                fieldGmail.getText(),
+                fieldName.getText(),
+                fieldSurname.getText(),
+                tel,
+                card,
+                comboGender.getValue()
+        );
+        boolean exists=false;
+        if (exists= bd.existsUsername(fieldUser.getText())){
+            System.out.println("Usuario ya existente");
+            showError("Usuario ya existente");
+        }
+        else{
+          boolean creado = bd.insertUser(nuevoUser);
+          
+          if (creado){
+              System.out.println("Creado correctamente");
+              showSuccess("Usuario creado correctamente");
+              clearFields();
+          }
+        }
+    }
+    }
+    
+    private void showError(String mensaje) {
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    private void showSuccess(String mensaje) {
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
+        alert.setTitle("Éxito");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+    private void clearFields(){
+        fieldUser.setText("");
+        fieldName.setText("");
+        fieldSurname.setText("");
+        fieldGmail.setText("");
+        fieldTel.setText("");
+        fieldPass.setText("");
+        fieldPass2.setText("");
+        fieldCard.setText("");
+    }
+
+
     
 }
